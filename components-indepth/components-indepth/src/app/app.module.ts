2fdbed80ff59eccabdad247f4dea39394ba77958
AzureAD\MykellenPeixoto <mykellen.peixoto@inovretail.com>
2024-05-27 14:52:02 +0100

    feat: app's first structure

--- components-indepth/components-indepth/src/app/app.module.ts
+++ components-indepth/components-indepth/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogListComponent,
+    BlogPostTileComponent,
+    TruncatePipe,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [TruncatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
